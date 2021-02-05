a9d77afb3a4f1e015d5667b7e85db15e78cd2539
Alexander Bai <vhurryharry@gmail.com>
2021-02-05 11:11:45 -0500

    Updated token contract

--- eFOIL.sol
+++ eFOIL.sol
@@ -1,10 +1,8 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 /**
  *Submitted for verification at Etherscan.io on 2021-01-12
  */
 
-pragma solidity ^0.7.4;
+pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
@@ -25,23 +23,23 @@ contract TokenMintERC20Token is ERC20 {
 
     /**
      * @dev Constructor.
-     * @param name_ name of the token
-     * @param symbol_ symbol of the token, 3-4 chars is recommended
-     * @param decimals_ number of decimal places of one token unit, 18 is widely used
+     * @param name name of the token
+     * @param symbol symbol of the token, 3-4 chars is recommended
+     * @param decimals number of decimal places of one token unit, 18 is widely used
      * @param totalSupply total supply of tokens in lowest units (depending on decimals)
      * @param tokenOwnerAddress address that gets 100% of token supply
      */
     constructor(
-        string memory name_,
-        string memory symbol_,
-        uint8 decimals_,
+        string memory name,
+        string memory symbol,
+        uint8 decimals,
         uint256 totalSupply,
         address payable feeReceiver,
         address tokenOwnerAddress
-    ) payable ERC20(name_, symbol_) {
-        _name = name_;
-        _symbol = symbol_;
-        _decimals = decimals_;
+    ) public payable {
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
 
         // set tokenOwnerAddress as owner of all tokens
         _mint(tokenOwnerAddress, totalSupply);
@@ -63,21 +61,21 @@ contract TokenMintERC20Token is ERC20 {
     /**
      * @return the name of the token.
      */
-    function name() public view override returns (string memory) {
+    function name() public view returns (string memory) {
         return _name;
     }
 
     /**
      * @return the symbol of the token.
      */
-    function symbol() public view override returns (string memory) {
+    function symbol() public view returns (string memory) {
         return _symbol;
     }
 
     /**
      * @return the number of decimals of the token.
      */
-    function decimals() public view override returns (uint8) {
+    function decimals() public view returns (uint8) {
         return _decimals;
     }
 }
